3fbe506490302f56107a40d05878c1b290347ec4
Audrey Alcantara <audreyalcantara@Audreys-MBP-10.lan>
2024-03-13 08:51:29 -0400

    refactored form function

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ document.addEventListener('DOMContentLoaded', function () {
         var formData = new FormData(form);
         var degrees = formData.get('userInput');
         var tempType = formData.get('temperature');
-
         validate(degrees);
-        var convertedValue = 0;
-        if (tempType.valueOf() === "celsius") {
-            var num = parseInt(degrees);
-            convertedValue = fahrenheitToCelsius(num);
-        }
-        if (tempType.valueOf() === "fahrenheit") {
-            var num = parseInt(degrees);
-            convertedValue = celsiusToFahrenheit(num);
-        }
+        var convertedValue = convert(tempType, degrees);
+        // let convertedValue: number = 0;
+        // if( tempType.valueOf() === "celsius"){
+        //     const num = parseInt(degrees)
+        //     convertedValue = fahrenheitToCelsius(num);
+        // }
+        // if( tempType.valueOf() === "fahrenheit"){
+        //     const num = parseInt(degrees)
+        //     convertedValue = celsiusToFahrenheit(num);
+        // }
         conversion.innerHTML = "Converted Temperature: ".concat(convertedValue.toFixed(2));
     });
     function fahrenheitToCelsius(fahrenheit) {
@@ -30,5 +30,17 @@ document.addEventListener('DOMContentLoaded', function () {
         if (regex.test(input) || input == "") {
             conversion.innerHTML = "Please enter non-numeric characters to convert";
         }
+    }
+    function convert(tempType, degrees) {
+        var convertedValue = 0;
+        if (tempType.valueOf() === "celsius") {
+            var num = parseInt(degrees);
+            convertedValue = fahrenheitToCelsius(num);
+        }
+        if (tempType.valueOf() === "fahrenheit") {
+            var num = parseInt(degrees);
+            convertedValue = celsiusToFahrenheit(num);
+        }
+        return convertedValue;
     }
 });
--- script.ts
+++ script.ts
@@ -7,28 +7,9 @@ document.addEventListener('DOMContentLoaded', function (){
         const formData = new FormData(form)
         const degrees = formData.get('userInput') as string;
         const tempType = formData.get('temperature') as string;
-
-        // if(!degrees){
-        //     console.log("please enter a value to convert")
-        //     return;
-        // }
-
         validate(degrees);
-
-        let convertedValue: number = 0;
-
-        if( tempType.valueOf() === "celsius"){
-            const num = parseInt(degrees)
-            convertedValue = fahrenheitToCelsius(num);
-        }
-
-        if( tempType.valueOf() === "fahrenheit"){
-            const num = parseInt(degrees)
-            convertedValue = celsiusToFahrenheit(num);
-        }
-
+        const convertedValue = convert(tempType, degrees);
         conversion.innerHTML = `Converted Temperature: ${convertedValue.toFixed(2)}`;
-        
     })
 
     function fahrenheitToCelsius(fahrenheit: number): number {
@@ -44,6 +25,21 @@ document.addEventListener('DOMContentLoaded', function (){
         if(regex.test(input) || input == ""){
             conversion.innerHTML = "Please enter non-numeric characters to convert"
         }
+    }
+
+    function convert(tempType: string, degrees: string): number {
+        let convertedValue: number = 0;
+
+        if( tempType.valueOf() === "celsius"){
+            const num = parseInt(degrees)
+            convertedValue = fahrenheitToCelsius(num);
+        }
+
+        if( tempType.valueOf() === "fahrenheit"){
+            const num = parseInt(degrees)
+            convertedValue = celsiusToFahrenheit(num);
+        }
+        return convertedValue;
     }
 
 })
